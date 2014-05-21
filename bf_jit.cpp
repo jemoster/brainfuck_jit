da8622b334cd6d4099ae0d0a39557affeafc9e07
joe <jemoster@gmail.com>
2014-05-21 00:14:20 -0700

    fixed missing logic on '[' where *ptr==0

--- bf_jit.cpp
+++ bf_jit.cpp
@@ -2,21 +2,20 @@
 //
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using std::cin;
 using std::cout;
  
 int main() {
-    char dat[30000];
+    char dat[40000];
     char *ptr=dat;
     char x;
 
-    std::vector<int> loops;
+    std::stack<int> loops;
     std::vector<char> stack;
 
-    while(cin >> x) {
-        if(x=='<' || x=='>' || x=='+' || x=='-' || x=='.' || x==',' || x=='[' || x==']') stack.push_back(x);
-    }
+    while(cin >> x) stack.push_back(x);
 
     int i=0;
     while(i<stack.size()){
@@ -29,19 +28,26 @@ int main() {
         else if(x=='.'){cout<< *ptr;}
         else if(x==','){cin >> *ptr;}
         else if(x=='['){
-        	loops.push_back(i);
+            if(*ptr!=0){
+                loops.push(i);
+            } else {
+                int bCounter = 0;
+                i++;
+                while(!(stack[i]==']' && bCounter==0)){
+                    if(stack[i]=='[') bCounter++;
+                    if(stack[i]==']') bCounter--;
+                    i++;
+                }
+            }
         }
         else if(x==']'){
-            if(*ptr==0){
-                loops.pop_back();
+            if((*ptr)==0){
+                loops.pop();
             } else {
-                i = loops.back();
+                i = loops.top();
             }
         }
         i++;
     }
-
-
-    cout<<std::endl;
     return 0;
 }
