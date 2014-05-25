cbd8f45ac01c7fb681fb6fffa6de77892d80ee0e
Joe <jemoster@gmail.com>
2014-05-25 00:30:02 -0700

    fixed setZero, added inc and add opts, added program printing

--- bf_jit.cpp
+++ bf_jit.cpp
@@ -6,12 +6,14 @@
 
 using std::cin;
 using std::cout;
+using std::endl;
 
-enum cmds {move, change, sLoop, eLoop, input, output, setZero, end};
+enum cmds {move, change, sLoop, eLoop, input, output, setZero, inc, add, end};
 
 struct inst {
     char cmd;
     int param;
+    int param2;
 } ;
 
 bool readInstruction(char x, inst &tmpInst){
@@ -47,53 +49,75 @@ bool readInstruction(char x, inst &tmpInst){
      return false;
 }
  
-void optimizeSetZero(std::vector<inst> program){
+void optimizeSetZero(std::vector<inst> &program){
     for(int i=0; i<program.size(); i++){
         if(program[i].cmd == sLoop & program[i+2].cmd == eLoop){
             if(program[i+1].cmd == change & program[i+1].param == -1){
                 program[i].cmd = setZero;
-                program.erase(program.begin()+i+1,program.begin()+i+2);
+                program.erase(program.begin()+i+1,program.begin()+i+2+1);
             }
         }
     }
     return;
 }
 
-int main() {
-    char dat[30000];
-    char *ptr=dat;
-    static void *jmp[] = { &&iMove, &&iChange , &&iSLoop, &&iELoop, &&iInput, &&iOutput, &&iSetZero, &&iEnd};
-    std::vector<inst> program;
+void optimizeAdd(std::vector<inst> &program){
+    for(int i=0; i<program.size(); i++){
+        if(program[i].cmd == sLoop){
+            for(int j=1; j<program.size(); j++){
+                if(program[i+j].cmd == eLoop){
+                    
+                    std::vector<inst> truncated;
 
-    char x;
-    //Load first valid instruction
-    inst firstInst;
-    while(cin>>x){
-        if(readInstruction(x, firstInst)){
-            program.push_back(firstInst);
-            break;
-        }
-    }
-
-    while(cin >> x){
-        inst tmpInst;
-        if(readInstruction(x, tmpInst)){
-            //Merge repeated commands
-            if(tmpInst.cmd == program.back().cmd && (tmpInst.cmd==move || tmpInst.cmd==change)){
-                program.back().param += tmpInst.param;
-            } else {
-                program.push_back(tmpInst);
+                    //scan root loop for side-effects
+                    bool isValid = true;
+                    bool hasDec = false;
+                    for(int k=1; k<j; k++){
+                        if(program[i+k].cmd==change){
+                            if(program[i+k].param!=-1){
+                                isValid=false;
+                            } else {
+                                hasDec = true;
+                            }
+                        } else if (program[i+k].cmd==inc) {
+                            if(!(program[i+k].param2==1 || program[i+k].param2==-1)){
+                                isValid=false;
+                            } else {
+                                truncated.push_back(program[i+k]);
+                            }
+                        } else {
+                            isValid=false;
+                        }
+                    }
+                    if(isValid && hasDec){
+                        for(int k=0; k<truncated.size(); k++){
+                            program[i+k].cmd = add;
+                            program[i+k].param = truncated[k].param;
+                        }
+                        program[i+truncated.size()].cmd = setZero;
+                        program.erase(program.begin()+i+truncated.size()+1, program.begin()+i+j+1);
+                    }
+                } else if(program[i+j].cmd == sLoop)j=program.size();
             }
+            //scan:
         }
     }
+    return;
+}
 
-    {
-        inst tmpInst;
-        tmpInst.cmd = end;
-        program.push_back(tmpInst);
+void optimizeInc(std::vector<inst> &program){
+    for(int i=0; i<program.size(); i++){
+        if(program[i].cmd==move & program[i+1].cmd==change & program[i+2].cmd==move & program[i].param==program[i+2].param){
+            program[i].cmd = inc;
+            program[i].param = program[i].param;
+            program[i].param2 = program[i+1].param;
+            program.erase(program.begin()+i+1,program.begin()+i+2+1);
+        }
     }
+    return;
+}
 
-    
+void linkLoops(std::vector<inst> &program){
     //Link Loops
     for( int j=0; j<program.size(); j++){
         if(program[j].cmd == sLoop){
@@ -112,10 +136,32 @@ int main() {
         }
 
     }
-
-    optimizeSetZero(program);
-
+}
+         
+void printProgram(const std::vector<inst> program){
+    int k = 0;
+    for(int j=0; j<program.size(); j++){
+        cout<<(j+k)<<": ";
+        switch(program[j].cmd){
+            case 0: cout<<"move"; break;
+            case 1: cout<<"change"; break;
+            case 2: cout<<"sLoop"; break;
+            case 3: cout<<"eLoop"; break;
+            case 4: cout<<"input"; break;
+            case 5: cout<<"output"; break;
+            case 6: cout<<"setZero"; k+=2; break;
+            case 7: cout<<"inc"; break;
+            case 8: cout<<"add"; break;
+            case 9: cout<<"end"; break;
+            default: cout<<"unknown"; break;
+        }
+        cout<<",\t"<<program[j].param<<",\t"<<program[j].param2<<endl;
+    }
+}
+void execute(const std::vector<inst> program, char* ptr){
+    static void *jmp[] = { &&iMove, &&iChange , &&iSLoop, &&iELoop, &&iInput, &&iOutput, &&iSetZero, &&iInc, &&iAdd, &&iEnd};
     int i=0;
+    //goto iEnd;
     goto *jmp[program[i].cmd];
     
     iMove: 
@@ -152,7 +198,59 @@ int main() {
     *ptr=0;
     i++;
     goto *jmp[program[i].cmd];
+
+    iAdd:
+    *(ptr+program[i].param) += *ptr;
+    i++;
+    goto *jmp[program[i].cmd];
     
+    iInc:
+    *(ptr+program[i].param) = *(ptr+program[i].param) + program[i].param2;
+    i++;
+    goto *jmp[program[i].cmd];
+
     iEnd:
-    return 0;
+    return;
+}
+
+int main() {
+    char dat[30000];
+    char *ptr=dat;
+    std::vector<inst> program;
+
+    char x;
+    //Load first valid instruction
+    inst firstInst;
+    while(cin>>x){
+        if(readInstruction(x, firstInst)){
+            program.push_back(firstInst);
+            break;
+        }
+    }
+
+    while(cin >> x){
+        inst tmpInst;
+        if(readInstruction(x, tmpInst)){
+            //Merge repeated commands
+            if(tmpInst.cmd == program.back().cmd && (tmpInst.cmd==move || tmpInst.cmd==change)){
+                program.back().param += tmpInst.param;
+            } else {
+                program.push_back(tmpInst);
+            }
+        }
+    }
+
+    {
+        inst tmpInst;
+        tmpInst.cmd = end;
+        program.push_back(tmpInst);
+    }
+
+    
+    optimizeSetZero(program);
+    //optimizeInc(program);
+    //optimizeAdd(program);
+    linkLoops(program);
+
+    execute(program, ptr);
 }
